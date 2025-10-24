4c76288bf661d6cb90b15978b9dfbc99f8225cbc
VextorHorizon <Coptersrichanth@gmail.com>
2025-10-24 23:04:14 +0700

    C?

---
+++ varlearn.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int num = 100;
+    float height = 160.5;
+    char grade = 'D';
+
+    printf("Number: %d\n", num);
+    printf("Height: %.f\n", height);
+    printf("Grade: %c\n", grade);
+
+    return 0;
+}
