5ddf35e0fbcd9ffc4964e480429ec0b9addf6ed3
VextorHorizon <Coptersrichanth@gmail.com>
2025-10-25 21:24:22 +0700

    Include user name input and display
    Added functionality to input and display user's name.

--- varlearn.c
+++ varlearn.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
 int main() {
+
+    char name[30];
     int num = 100;
     float height = 160.5;
     char grade = 'D';
 
+    
+    printf("Your name: ");
+    scanf("%29s", name);
+
+    printf("Your name is: %s\n", name);
     printf("Number: %d\n", num);
     printf("Height: %.f\n", height);
     printf("Grade: %c\n", grade);
