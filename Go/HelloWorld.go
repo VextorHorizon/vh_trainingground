b81795e8c1606dd5c112865fee4cbe1f00766024
VextorHorizon <Coptersrichanth@gmail.com>
2026-01-10 00:04:31 +0700

    Add files via upload

---
+++ Go/HelloWorld.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+
+	var numA int = 10
+	var numB int = 12
+	var result int = numA + numB
+
+	fmt.Printf("Number is %d", result)
+}
