3c82965bc9378300352bcff5425dcb6aa0d72380
VextorHorizon <Coptersrichanth@gmail.com>
2026-01-21 23:48:55 +0700

    Bug code, my brain refused to fix before midnight

--- Go/fundamental/fileinfo.go
+++ Go/fundamental/fileinfo.go
@@ -8,12 +8,13 @@ import (
 
 func main() {
 
-	files, err := os.ReadDir(".") // มันจะให้ค่าออกมาเป็น Object เช่น filenamescan.go ไม่ใช่แค่ชื่อแต่จะเป็นข้อมูลที่อยู่ใน Object นั้นแบบจะได้ ชื่อ วันที่แก้ไขไฟล์ ขนาด แพ็คอยู่ใน Object นั้นๆเลย
+	files, err := os.ReadDir(".")
 	if err != nil {
 		log.Fatal(err) // ถ้า error ให้ log ว่า error อะไร แล้วก้กระโดดออกหน้าต่าง
 	}
 	// fmt.Printf("%v", files)
 	// fmt.Println(len(files))
+	var larGthan10 []string
 	for _, file := range files {
 		info, err := file.Info()
 		if err != nil {
@@ -21,13 +22,15 @@ func main() {
 		}
 		// fmt.Println(info)
 
-		filemb := (float64(info.Size()) / 1024 / 1024) // (1024 / 1024) คือ หารเลข byte ด้วย 1024 * 1024 (1,048,576) เพื่อให้ได้เลข Mebibytes (MiB) ซึ่งเป็น Binary หรือว่าเลขฐานสอง 
+		filemb := (float64(info.Size()) / 1024 / 1024)
 		if filemb < 1 {
 			fmt.Printf("File Name: %-25s File Size: %10d Byte\n", info.Name(), info.Size())
+		} else if filemb >= 10 {
+			larGthan10 = append(larGthan10, info.Name())
+			fmt.Println(larGthan10)
 		} else {
 			fmt.Printf("File Name: %-25s File Size: %10.2f Megabyte\n", info.Name(), filemb)
 		}
 	}
 
 }
-
