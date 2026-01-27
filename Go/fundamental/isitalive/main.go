612c9da395271ffc97ce7b39cfdeac7869ceb428
VextorHorizon <Coptersrichanth@gmail.com>
2026-01-27 20:00:04 +0700

    Enhance request function with error handling
    Added error handling and status code check in request function.

--- Go/fundamental/isitalive/main.go
+++ Go/fundamental/isitalive/main.go
@@ -3,9 +3,34 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"time"
+	"log"
+	"os"
 )
 
 func request(url string) {
 	response, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if response.StatusCode == 200{
+		fmt.Println("Website is alive!")
+	} else {
+		fmt.Println("Website is down?")
+	}
+	// fmt.Println(response.StatusCode)
+
+}
+
+func main(){
+
+	args := os.Args
+	userInput := args[1]
+
+	if userInput == "" {
+		fmt.Println("Empty")
+	}
+	request(userInput)
+	
+	
+
 }
