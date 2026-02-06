d3b86ec3598973b25e6395aa8a006faa38e5e760
VextorHorizon <Coptersrichanth@gmail.com>
2026-02-06 21:38:28 +0700

    Update dontknowwhattodo.go

--- Go/fundamental/ext/dontknowwhattodo.go
+++ Go/fundamental/ext/dontknowwhattodo.go
@@ -4,8 +4,21 @@ import (
 	"fmt"
 )
 
+type Task struct {
+	Name   string
+	isDone bool
+}
+
 func main() {
 
+	Task1 := Task{
+		Name:   "My heart",
+		isDone: false,
+	}
 	fmt.Println("Don't know what to do today")
+
+	if Task1.isDone == false {
+		fmt.Println("Heart broken?")
+	}
 
 }
