83e5c478e60412370111b49af1b1153bb0805dc0
VextorHorizon <Coptersrichanth@gmail.com>
2026-03-07 23:07:52 +0700

    Refactor Guitar class constructor formatting

--- Typescript/fundamental/cheatsheet/constructorWTH.ts
+++ Typescript/fundamental/cheatsheet/constructorWTH.ts
@@ -1,13 +1,9 @@
 class Guitar {
   brand: string;
 
-  // นี่คือ Constructor!
   constructor(brandName: string) {
     this.brand = brandName; 
     console.log("กำลังสร้างกีต้าร์ยี่ห้อ " + this.brand);
   }
 }
-
-// ตอนแกพิมพ์คำว่า new... 
-// มันจะไปเรียก Constructor ข้างบนมาทำงานทันที!
 const myGuitar = new Guitar("Music Man");
