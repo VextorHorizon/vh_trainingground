149c7fd071caa6b346df137402e9075ff2bc5828
VextorHorizon <Coptersrichanth@gmail.com>
2026-06-14 01:38:45 +0700

    update func_wit comments

--- rust/6-11-26/func_wit.rs
+++ rust/6-11-26/func_wit.rs
@@ -1,13 +1,17 @@
 fn change_bool(status: &mut bool){
     println!("{status}");
-    *status = true
+    *status = true // *status = เนื้อหาของ status
 }
-
 
 fn main(){
     let mut booling = false;
     let longtiju_booling = &booling;
-    change_bool(&mut booling); // ให้สังเกตุ & ให้ดี มันคือ pointer 
+    // println!("{longtiju_booling}");
+    change_bool(&mut booling);  
     println!("{longtiju_booling}");
     println!("{booling}");
 }
+
+
+// borrowing concept นี่หว่า concept คือ ใครจะยืม(ชี้ไปที่ตัวแปรต้นทาง)ไปอ่านก็ได้ แต่ว่ามีคนแก้ได้แค่คนเดียว!(&mut) 
+// คนนั้นคนที่ขอยืมไปเพื่อแก้ คนที่ 1 คนอื่นจะไปแก้หรือทำอะไรระหว่างคนที่ 1 แก้ไม่ได้!
